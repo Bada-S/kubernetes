a05eca4d9c79eaad955d7cd1ef92a91dafc43e1b
Bada-S <jamessopkin@gmail.com>
2022-01-20 09:53:41 -0600

    sadfasd

--- flow.py
+++ flow.py
@@ -1,16 +1,23 @@
 from prefect import Flow, task
 from prefect.executors import DaskExecutor
-from dask_kubernetes import KubeCluster, make_pod_spec
+
+# from dask_kubernetes import KubeCluster, make_pod_spec
 from prefect.storage import GitHub
 from prefect.run_configs import KubernetesRun
+
+import time
 
 
 @task
 def task():
+    time.sleep(5)
     return 1 + 1
 
 
-with Flow() as flow:
+with Flow(name="kube") as flow:
     task()
 
-flow.run(storage=storage)
+
+flow.run_config = KubernetesRun()
+flow.storage = GitHub(repo="Bada-S/kubernetes", path="flow.py")
+flow.run()
